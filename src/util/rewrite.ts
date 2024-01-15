8d8b69fd55705cff3bb0f2dcfb3da6d8febbdc0e
Stephen Fraser <stephen.fraser@digirati.com>
2024-01-15 10:54:02 +0000

    Fixed type

--- src/util/rewrite.ts
+++ src/util/rewrite.ts
@@ -1,11 +1,9 @@
-import { ParsedResource } from "./store.ts";
-
 export interface Rewrite {
   id: string;
   name: string;
   types: string[];
   rewrite?: (
     slug: string,
-    resource: { id: string; type: string },
+    resource: { id?: string; type: string },
   ) => string | Promise<string>;
 }
